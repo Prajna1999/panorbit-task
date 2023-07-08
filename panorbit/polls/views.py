c02bf33f8e408420a8da9bd83c0e01e4db246d91
Prajna1999 <gituprajna20@gmail.com>
2023-07-08 19:22:11 +0530

    add views

--- panorbit/polls/views.py
+++ panorbit/polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 # calls this specified view 
 def index(request):
     return HttpResponse("Hello, you are at the poll index.")
+
+def detail(request,question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response="You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
